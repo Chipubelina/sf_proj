5fa1f5e8f5d4bb3d11c669561637d09a0a30621b
Chipubelina <griaga@bk.ru>
2022-01-29 22:03:13 +0300

    proj1

--- project_0/game.py
+++ project_0/game.py
@@ -1,37 +1,51 @@
 import numpy as np
 
-number = np.random.randint(1, 101)
-
-
 def random_predict(number: int = 1) -> int:
+    """Используя алгоритм бинарного поиска угадывает число
+       Принимает загаданное число из отрезка [1;100] и возвращает кол-во попыток.
     
-    count = 0
+
+    Args:
+        number (int, optional): Загаданное число. Defaults to 1.
+
+    Returns:
+        int: Количество попыток
+    """
+    count = 1
+    low = 1
+    high = 100
+    predict_number = 50
 
     while True:
-        count += 1
-        predict_number = np.random.randint(1, 101)  # предполагаемое число
+        if predict_number > number:
+            high = predict_number - 1
+        elif predict_number < number:
+            low = predict_number + 1
+            
         if number == predict_number:
-            break  # выход из цикла, если угадали
-    return count
+            return count
+
+        predict_number = (low+high) // 2
+        count += 1
 
 def score_game(random_predict) -> int:
     """За какое количество попыток в среднем за 1000 подходов угадывает наш алгоритм
 
     Args:
         random_predict ([type]): функция угадывания
-\
+
     Returns:
         int: среднее количество попыток
     """
-
-    count_ls = [] # список для сохранения количества попыток
-    np.random.seed(1) # фиксируем сид для вопроизводимости
-    random_array = np.random.randint(1, 101, size=(1000)) # загадали список чисел
+    count_ls = []
+    np.random.seed(2022)  # фиксируем сид для воспроизводимости
+    random_array = np.random.randint(1, 101, size=(1000)) # список для чисел  
 
     for number in random_array:
         count_ls.append(random_predict(number))
 
-    score = int(np.mean(count_ls)) # находим среднее количество попыток
+    score = int(np.mean(count_ls))
+    return score
+    
 
-    print(f'Ваш алгоритм угадывает число в среднем за: {score} попыток')
-    return(score)
+print(score_game(random_predict))
